6b30b5a7f708d1a41eac481fb3c23e237b812c71
Phuongntctb <phuongnguyen@Phuongs-MacBook-Pro.local>
2015-03-18 11:41:25 +0900

    Merge branch 'Testmerge'
    Conflicts:
    	untitled folder/A00_BasicAct.java

--- untitled folder/A00_BasicAct.java
+++ untitled folder/A00_BasicAct.java
@@ -1,5 +1,19 @@
 fdskajfkldsajflksdjaklfjlsdkajfklsdjafkldjsaklfjasdlkjflksa
 
+<<<<<<< HEAD
 Test master
 
 Test masetr
+=======
+<<<<<<< HEAD
+
+
+Test merge nhe!!!
+=======
+Test master
+
+Test masetr
+>>>>>>> master
+
+child that do nhe!!!!!
+>>>>>>> Testmerge
